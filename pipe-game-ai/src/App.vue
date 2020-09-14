e8a774596fc966c3ec481781b5a1bd555f89c007
godprogrammer <game47you@gmail.com>
2020-09-14 08:53:27 +0700

    add vue project

---
+++ pipe-game-ai/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div id="app">
+  <v-app>
+   <router-view ></router-view>
+  </v-app>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ pipe-game-ai/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <span class="bg"></span>
+    <div class="content">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+<style scoped>
+.bg {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: url("../assets/background.svg") no-repeat center center;
+  background-size: cover;
+  transform: scale(1);
+  z-index: 0;
+}
+.content {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+}
+</style>
+
